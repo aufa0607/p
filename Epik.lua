637d09bd8c711580e4ca6a7f5349caa74c1fc57c
aufa0607 <129158103+aufa0607@users.noreply.github.com>
2025-02-23 16:16:30 +0700

    Update Epik.lua

--- Epik.lua
+++ Epik.lua
@@ -45,12 +45,12 @@ local Section = mainTab:CreateSection("main")
 
 local Slider = mainTab:CreateSlider({
    Name = "speed boost",
-   Range = {0, 100},
-   Increment = 10,
+   Range = {0, 300},
+   Increment = 1,
    Suffix = "speed",
-   CurrentValue = 10,
+   CurrentValue = 16,
    Flag = "Slider1", -- A flag is the identifier for the configuration file, make sure every element has a different flag if you're using configuration saving to ensure no overlaps
    Callback = function(Value)
-   Game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = (value)
+         Game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = (value)
    end,
 })
